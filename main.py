675f1df2599dd7b98ecde6b08775953b37e9565f
Mazin <mazinhussain03@gmail.com>
2025-07-14 21:33:07 +0400

    Tested different feature cases (High score = 71.2% Accuracy)

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from sklearn.feature_selection import mutual_info_regression
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
 import matplotlib as plt
+from sklearn.metrics import mean_absolute_error
 
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
 
-X_train = train_data.drop(columns=['Transported'])
-y_train = train_data['Transported']
+X = train_data.drop(columns=['Transported'])
+y = train_data['Transported']
 X_test = test_data.copy()
 
 #Breaking Down Complex Features:
@@ -18,37 +20,39 @@ X_test = test_data.copy()
 #Feature correction (fc):
 
 #PassengerId can be split into Group and Ranking
-X_train[['Group','Ranking']] = (X_train['PassengerId'].str.split("_", expand=True))
+X[['Group','Ranking']] = (X['PassengerId'].str.split("_", expand=True))
 X_test[['Group','Ranking']] = (X_test['PassengerId'].str.split("_", expand=True))
 
 #Cabin can be split into Deck, Num, and Side
-X_train[['Deck', 'Num', 'Side']] = (X_train['Cabin'].str.split("/", expand=True))
+X[['Deck', 'Num', 'Side']] = (X['Cabin'].str.split("/", expand=True))
 X_test[['Deck', 'Num', 'Side']] = (X_test['Cabin'].str.split("/", expand=True))
 
 #drop the originals for their more readable counterparts
-X_train = X_train.drop(['PassengerId', 'Cabin', 'Name'], axis=1)
+X = X.drop(['PassengerId', 'Cabin', 'Name'], axis=1)
 X_test = X_test.drop(['PassengerId', 'Cabin', 'Name'], axis=1)
 
 #was type == object, converted to type == int for better use
-X_train['Group'] = X_train['Group'].astype(int)
+X['Group'] = X['Group'].astype(int)
 X_test['Group'] = X_test['Group'].astype(int)
+
+X_train, X_val, y_train, y_val = train_test_split(X,y,test_size=0.2,random_state=0)
 
 #Applying Ordinal Encoding
 label_X_train = X_train.copy()
-label_X_test = X_test.copy()
+label_X_val = X_val.copy()
 object_cols = [cols for cols in X_train.columns if X_train[cols].dtypes == 'object']
 
 ordinal_encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
 label_X_train[object_cols] = ordinal_encoder.fit_transform(X_train[object_cols])
-label_X_test[object_cols] = ordinal_encoder.transform(X_test[object_cols])
+label_X_val[object_cols] = ordinal_encoder.transform(X_val[object_cols])
 
 #Applying Simple Imputer
 my_imputer = SimpleImputer(strategy='mean')
 imputed_X_train= pd.DataFrame(my_imputer.fit_transform(label_X_train))
-imputed_X_test = pd.DataFrame(my_imputer.transform(label_X_test))
+imputed_X_val = pd.DataFrame(my_imputer.transform(label_X_val))
 
 imputed_X_train.columns = label_X_train.columns
-imputed_X_test.columns = label_X_test.columns
+imputed_X_val.columns = label_X_val.columns
 
 #Testing to see which feature is relevant via. MI scores
 
@@ -69,4 +73,41 @@ mi_scores = make_mi_scores(imputed_X_train, y_train, discrete_features)
 
 # Test 1: Using only ['Group','CryoSleep']
 
+"""
+X_features = imputed_X_train[['Group', 'CryoSleep']]
+X_val_features = imputed_X_val[['Group', 'CryoSleep']]
+my_model = XGBRegressor(n=500)
+my_model.fit(X_features,y_train, verbose = False)
+
+y_pred = my_model.predict(X_val_features)
+
+mean_absolute_error(y_pred, y_val)
+"""
+
+#MAE = 0.388, Accuracy = 1-0.388 = 62% (Not Bad, can be better)
+
+
 # Test2: Using ['Group','CryoSleep'] and ['Spa','RoomService','VRDeck','ShoppingMall','FoodCourt']
+
+'''
+X_features = imputed_X_train[['Group', 'CryoSleep', 'Spa','RoomService','VRDeck','ShoppingMall','FoodCourt']]
+X_val_features = imputed_X_val[['Group', 'CryoSleep','Spa','RoomService','VRDeck','ShoppingMall','FoodCourt']]
+my_model = XGBRegressor(n=300)
+my_model.fit(X_features,y_train, verbose = False)
+
+y_pred = my_model.predict(X_val_features)
+
+mean_absolute_error(y_pred, y_val)
+'''
+# MAE = 0.312, Accuracy = 68.7%
+
+#Test3: Use all features
+"""
+my_model = XGBRegressor(n=300)
+my_model.fit(imputed_X_train,y_train, verbose = False)
+
+y_pred = my_model.predict(imputed_X_val)
+
+mean_absolute_error(y_pred, y_val)
+"""
+#MAE = 0.282, Accuracy = 71.2%
